5c76e27b27260506832f9563fa26f93fcf7fa47b
Clos Devop <31907938+cgarcia101015@users.noreply.github.com>
2018-12-23 10:12:13 -0500

    added all items

--- bamazon.sql
+++ bamazon.sql
@@ -28,8 +28,31 @@ PRIMARY KEY (item_id)
 INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
 VALUES (1, "Cannon EOS Rebel T6", "Electronics", 429, 176);
 
-
 INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("Bose QuietComfort 35 Wireless Headphones", "Electronics", 299, 1206);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("TRUFF Hot Sauce", "Food", 18, 423);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Philips Smoke-less Indoor BBQ Grill", "Food", 250, 385);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("ThermoPro TP20 Wireless Remote Digital Cooking Food Meat Thermometer", "Cooking", 60, 52);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("OFM Essentials Leather Office Chair", "Furniture", 56, 731);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Louis Carpini G2 Violin Outfit 4/4 (Full) Size", "Musical Instruments", 600, 75);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Collapsible Dog Bowl", "Pet Supplies", 13, 261);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Tonka Toughest Mighty Dump Truck", "Toys & Games", 45, 154);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Walk-A-Long Puppy Wooden Toy", "Toys & Games", 24, 456);
 
 SELECT *FROM products;
